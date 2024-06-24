447bf218b5a97389e1a97ee5631c22785c504abb
david florczak <davidflorczak@davids-MacBook-Pro.local>
2024-06-24 16:53:23 +0200

    Added DDL + DML #2

---
+++ spot-me-database/Scripts/data.dml.sql
@@ -0,0 +1,5 @@
+DELETE FROM t_spots;
+
+INSERT INTO t_spots (spot_name, spot_lat, spot_lng, spot_desc) VALUES
+	('celtic clover', 48.86328976481655, 2.3482775111142575, 'un bar a guiness a chatelet'),
+	('The 1/3rd coffee bar', 35.70252709088118, 139.77220392300111, 'un bar a tokyo');
